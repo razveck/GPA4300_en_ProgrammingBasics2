41b54e6f343312888e0ebc32b2a4615f3e354725
razveck <razveck@gmail.com>
2021-01-05 11:39:20 +0000

    Added Events and Properties code

---
+++ Events/Events.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Events {
+
+	class Player{
+		private float _health;
+
+		//HealthBar healthBar;
+		//DynamicDifficulty difficulty;
+
+		public event Action<float> HealthChanged;
+
+		public void DealDamage(float damage){
+			_health -= damage;
+			//healthBar.ChangeHealth(Health);
+			//difficulty.ChangeDifficulty(Health);
+
+			//raise the event
+			HealthChanged(_health);
+		}
+	}
+
+	class HealthBar{
+
+		public Player Player;
+
+		public HealthBar(Player player) {
+			Player = player;
+			Player.HealthChanged += ChangeHealth;
+		}
+
+		public void ChangeHealth(float health){
+			//...
+		}
+	}
+
+	class DynamicDifficulty{
+		public Player Player;
+
+		public DynamicDifficulty(Player player) {
+			Player = player;
+			Player.HealthChanged += ChangeDifficulty;
+		}
+
+		public void ChangeDifficulty(float health){
+			//...
+		}
+	}
+
+	class Events {
+		static void Main(string[] args) {
+			Player player = new Player();
+			HealthBar healthbar = new HealthBar(player);
+			DynamicDifficulty difficulty = new DynamicDifficulty(player);
+
+			player.DealDamage(10);
+		}
+	}
+}
---
+++ Properties/Properties.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Properties {
+
+	class Person {
+		//backing field of the property
+		private string _name;
+		public string NameProperty
+		{
+			get
+			{
+				if(_name == null)
+					_name = "Default";
+				return _name;
+			}
+			set
+			{
+				_name = value;
+				Console.WriteLine($"The new name is {_name}");
+			}
+		}
+
+		public string GetName() {
+			return _name;
+		}
+
+		public void SetName(string value) {
+			_name = value;
+		}
+
+		//auto-property
+		public string AutoProperty { get; set; }
+
+		public string ReadOnlyProperty { get; private set; }
+
+		public string WriteOnlyProperty { private get; set; }
+
+		public string PropertyWithInitialValue { get; set; } = "BlaBla";
+
+
+		private bool PropertyWithArrows {
+			get => true;
+			set => Console.WriteLine("Set the value");
+		}
+
+		private string description;
+		public string Description { get => description; set => description = value; }
+
+		public Person() {
+			ReadOnlyProperty = "Readonly";
+		}
+
+	}
+	
+	class Properties {
+		static void Main(string[] args) {
+			Person person = new Person();
+			Console.WriteLine(person.NameProperty);
+
+			person.NameProperty = "Joao";
+
+			Console.WriteLine(person.ReadOnlyProperty);
+
+			person.Description = "This is a person";
+			Console.WriteLine(person.Description);
+
+		}
+	}
+}
